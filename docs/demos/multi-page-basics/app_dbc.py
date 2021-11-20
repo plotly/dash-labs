e1cfa27ad1af0247a33a7b5817ac542f97a70c7f
adam schroeder <adamschroeder.m@gmail.com>
2021-11-20 11:52:19 -0500

    make-darker-them2e

--- docs/demos/multi-page-basics/app_dbc.py
+++ docs/demos/multi-page-basics/app_dbc.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 
 
 app = dash.Dash(
-    __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.CYBORG]
+    __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.CYBRG]
 )
 
 navbar = dbc.NavbarSimple(
