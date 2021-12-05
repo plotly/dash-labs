ec80b61e3fc8d99f7f9d6e8bcb975f97a0e28bf9
AnnMarieW <amward@fastmail.com>
2021-12-05 11:19:09 -0700

    cleaned up demo apps

--- docs/demos/multi_page_basics/pages/home.py
+++ docs/demos/multi_page_basics/pages/home.py
@@ -4,10 +4,8 @@ from dash import html
 
 dash.register_page(
     __name__,
-  #  path="/",
     name="Analytic Apps",
     description="Welcome to my app",
-  #  order=0,
     redirect_from=["/old-home-page", "/v2"],
     extra_template_stuff="yup",
 )
--- docs/demos/multi_page_basics/pages/outlook.py
+++ docs/demos/multi_page_basics/pages/outlook.py
@@ -4,7 +4,6 @@ dash.register_page(
     __name__,
     title="Forward Outlook",
     description="This is the forward outlook",  # should accept callable too
-  #  order=2,
     path="/forward-outlook",
     image="birds.jpeg",
 )
